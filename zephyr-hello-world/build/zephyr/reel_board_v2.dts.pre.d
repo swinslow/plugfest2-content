4b244f23ea8fe90bdc55936fdcf6e140d81b9e49
Steve Winslow <steve@swinslow.net>
2021-05-30 13:11:25 -0400

    Add build files for Zephyr hello-world
    Signed-off-by: Steve Winslow <steve@swinslow.net>

---
+++ zephyr-hello-world/build/zephyr/reel_board_v2.dts.pre.d
@@ -0,0 +1,13 @@
+empty_file.o: \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/misc/empty_file.c \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/boards/arm/reel_board/reel_board_v2.dts \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/dts/arm/nordic/nrf52840_qiaa.dtsi \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/dts/common/mem.h \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/dts/arm/nordic/nrf52840.dtsi \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/include/dt-bindings/i2c/i2c.h \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/include/dt-bindings/gpio/gpio.h \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/dts/arm/nordic/override.dtsi \
+ /home/steve/programming/zephyr/zephyrproject/zephyr/boards/arm/reel_board/dts/reel_board.dtsi
